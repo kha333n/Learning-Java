5f89af96a628c047a5f73e28fadae5f925d19145
Usman Khan <kha333n@outlook.com>
2020-02-29 22:24:15 +0500

    Create DoWhile.java

---
+++ src/com/learning/java/DoWhile.java
@@ -0,0 +1,13 @@
+package com.learning.java;
+
+public class DoWhile
+{
+    public static void main(String [] args)
+    {
+        int counter = 1;
+        do {
+            System.out.printf("%d ",counter);
+            counter++;
+        }while (counter <= 10);
+    }
+}
