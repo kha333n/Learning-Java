1424dce0cb5fb7fac273d652875fc8cf3cc62256
Usman Khan <kha333n@outlook.com>
2020-02-29 14:34:56 +0500

    While Loop
    In fine GradeBook.java

--- src/com/learning/java/GradeBook.java
+++ src/com/learning/java/GradeBook.java
@@ -1,5 +1,7 @@
 package com.learning.java;
 
+import java.util.Scanner;
+
 public class GradeBook
 {
     private String course;
@@ -16,5 +18,21 @@ public class GradeBook
     public void DisplayMessage() {
         System.out.printf("Welcome to the grade book for \n %s! \n",course);
     }
+    public void determineClassAverage()
+    {
+        Scanner input = new Scanner(System.in);
+        int total = 0, gradeCounter = 1, grade, average;
+        while (gradeCounter <= 10)
+        {
+            System.out.print("Enter Grade: ");
+            grade = input.nextInt();
+            total = total + grade;
+            gradeCounter++;
+        }
+        average = total / 10;
+        System.out.printf("\nTotal of all 10 grades is %d \n",total);
+        System.out.printf("Class average is %d\n",average);
+    }
+
 }
 
--- src/com/learning/java/GradeBookTest.java
+++ src/com/learning/java/GradeBookTest.java
@@ -14,6 +14,7 @@ public class GradeBookTest
         System.out.println();
         myGradeBook.setCourse(course);
         myGradeBook.DisplayMessage();
+        myGradeBook.determineClassAverage();
     }
 }
 
