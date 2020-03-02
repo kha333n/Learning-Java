64b0e80b3413cbfa2771f8d1e445e6ee55e07fa5
Usman Khan <kha333n@outlook.com>
2020-03-02 13:24:18 +0500

    Update InitArray.java
    Initilizer List

--- src/com/learning/java/InitArray.java
+++ src/com/learning/java/InitArray.java
@@ -3,8 +3,11 @@ package com.learning.java;
 public class InitArray
 {
     public static void main(String[] args){
-        int [] array = new int[10];
+        //int [] array = new int[10];
         //Declare array
+
+        //Initializer List
+        int[] array = {10,20,30,40,50,60,70,80,90,100};
 
         System.out.printf("%s%8s\n","Index","Value");
         //Column heading
